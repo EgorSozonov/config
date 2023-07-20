45870658d7ce20ff48edbd4cf54e71b699860636
zrx <zrx@zrxcomp.sozonov.tech>
2023-07-21 00:42:58 +0300

    New nvim!

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,19 @@
--- Key mapping
-local map = require("utils").map
-local plugins = require("plugins")
-
+--{{{ Utils
+function map(mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
 local g = vim.g -- global variables
 local o = vim.o -- get or set options
 local go = vim.go -- get or set global options
 local wo = vim.wo -- window-scoped options
 local bo = vim.bo -- buffer-scoped options
+--}}}
+
+--{{{Settings
 vim.opt.foldmethod = "marker"
 vim.opt.wrap = true
 vim.opt.linebreak = true -- Stop words from being broken on wrap
@@ -22,11 +29,14 @@ o.expandtab = true
 o.smartindent = true
 vim.opt.clipboard = "unnamedplus" -- normal copy and paste via X clipboard
 
-g.mapleader=' '
-g.maplocalleader = ' '
+g.mapleader=','
+g.maplocalleader = ','
 
 wo.nu = true -- line numbers
 vim.cmd(":hi LineNr guibg=#000000 guifg=#ffffff")
+--}}}
+
+--{{{ Keybindings
 map("i", "jk", "<Esc>", { silent = true })
 map("i", "kj", "<Esc>", { silent = true })
 map("i", "<C-s>", "<C-o>:Update<CR>", { silent = true })
@@ -38,5 +48,18 @@ map("n", "<C-n>", ":bn<CR>", { silent = true }) -- next buffer in order
 map("n", "<C-p>", ":bp<CR>", { silent = true }) -- previous buffer in order
 map("n", "<C-3>", ":b#<CR>", { silent = true }) -- previous buffer you were in
 map("n", "<M-e>", ":Explore<CR>", { silent = true })
+--}}}
 
---plugins.require("leap").add_default_mappings()
+--{{{ Packages
+
+require "paq" {
+    "savq/paq-nvim", -- Let Paq manage itself
+    "nvim-lua/plenary.nvim",
+    "neovim/nvim-lspconfig",
+    { "ggandor/leap.nvim" },
+    "nvim-telescope/telescope.nvim"
+}
+require("leap").add_default_mappings()
+local telescope = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", telescope.find_files, {silent = true})
+--}}}
