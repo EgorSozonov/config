e2aa834264f66ff03150a66431ca2a042144e0b0
EgorSozonov <Egor.Sozonov.09@gmail.com>
2026-05-26 10:39:00 +0300

    New sound widget on waybar

---
+++ .config/waybar/mute.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/bash
+if  amixer sget Master | grep -q '\[on\]' ; then
+    echo '{"text": " ", "class": "on", "tooltip": "Sound is ON"}'
+else
+    echo '{"text": " ", "class": "off", "tooltip": "Muted"}'
+fi
