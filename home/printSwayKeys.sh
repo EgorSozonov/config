264b515f6458b5320c6e936ec701cad99be82ef3
EgorSozonov <Egor.Sozonov.09@gmail.com>
2026-07-14 12:42:27 +0300

    Sound toggling works

--- home/printSwayKeys.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-grep -E '^\s*(bindsym|bindcode)' ~/.config/sway/config | \
-   sed -e 's;\$mod;Win;g'  -e 's;^ \+;;' -e 's;bindsym ;;' -e 's;bindcode ;;' \
-      -e 's;--locked ;;' -e 's;\$left;<-;g' -e 's;\$right;->;g'  -e 's;\$up;^;g' -e 's;\$down;v;g' | \
-   sed 's;\([^ ]\+\) \(.*$\);\1 :: \2;'
-echo "" 
-echo "(use Win+c to close this window)" 
-
