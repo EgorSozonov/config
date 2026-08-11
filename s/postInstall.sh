69bfd8fd5904f2fe3aebe17d6c74a8f585e20a43
EgorSozonov <Egor.Sozonov.09@gmail.com>
2026-08-11 11:24:25 +0300

    Two-column wofi layout

--- s/postInstall.sh
+++ s/postInstall.sh
@@ -1,10 +1,11 @@
 #! /usr/bin/bash
 
 doas pacman -Syu apparmor pacman-contrib wl-clipboard
-doas pacman -Syu alsa-utils pipewire tree
+doas pacman -Syu alsa-utils pipewire tree man which jq
 
 doas pacman -Syu chrony
 sys enable --now chronyd
 sys start chronyd
 
+doas pacman -Syu guile make base-devel
 
