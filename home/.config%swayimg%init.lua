f9df19dd66fa048f90c7354d53b4bf30ba353028
EgorSozonov <Egor.Sozonov.09@gmail.com>
2026-08-12 16:05:06 +0300

    Fix

--- home/.config%swayimg%init.lua
+++ home/.config%swayimg%init.lua
@@ -1,8 +1,8 @@
 
 swayimg.gallery.thumb_size = 128 
-swayimg.gallery.pstore = true #store thumbnails in ~/.cache/swayimg
+swayimg.gallery.pstore = true --store thumbnails in ~/.cache/swayimg
 swayimg.viewer.preload = 3
-swayimg.viewer.history = 4 #how many just viewed images to keep in memory
+swayimg.viewer.history = 4 --how many just viewed images to keep in memory
 swayimg.gallery.on_key("h", function() swayimg.gallery.select("left") end)
 swayimg.gallery.on_key("l", function() swayimg.gallery.select("right") end)
 swayimg.gallery.on_key("k", function() swayimg.gallery.select("up") end)
