43c27ed52bc1394cdd1c92d5bd905a55bd21d1dc
EgorSozonov <Egor.Sozonov.09@gmail.com>
2026-08-14 13:33:57 +0300

    Minor update

--- s/postInstall.sh
+++ s/postInstall.sh
@@ -1,12 +1,16 @@
 #! /usr/bin/bash
 
-doas pacman -Syu apparmor pacman-contrib wl-clipboard sway-contrib
-doas pacman -Syu alsa-utils pipewire tree man which jq
+doas pacman -Syu apparmor pacman-contrib wl-clipboard sway-contrib otf-font-awesome
+doas pacman -Syu alsa-utils pipewire tree man which jq rsync
 
 doas pacman -Syu chrony
 sys enable --now chronyd
+sys enable --now iwd
+sys enable --now dnsmasq
 sys start chronyd
+sys start dnsmasq
+sys start iwd
 
-doas pacman -Syu guile make base-devel devtools
+doas pacman -Syu guile make base-devel devtools tig
 doas pacman -Syu impala id3v2 ffmpeg
 
