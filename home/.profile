1a14f05ca7353dceb6bf874904119e512e6e1e86
EgorSozonov <Egor.Sozonov.09@gmail.com>
2026-08-15 23:59:56 +0300

    More flexible restriction on user ns creation

--- home/.profile
+++ home/.profile
@@ -6,6 +6,7 @@ readonly PATH="/opt/bin:/usr/local/bin:/usr/bin"
 export LS_COLORS='di=1:fi=0:st=1:tw=1:ln=33:ex=\033[38;5;46:pi=0:so=0:bd=0:cd=0:or=31:ow=0'
 export HISTFILE="~/.local/state/.bash_history"
 export PSQL_HISTORY="~/.local/state/.psql_history"
+export GNUPGHOME="~/.config/gnupg"
 
 if [[ -z "$WAYLAND_DISPLAY" ]] && [[ -n "$XDG_VTNR" ]] && [[ "$XDG_VTNR" -eq 1 ]] ; then
     exec sway
--- s/postInstall.sh
+++ s/postInstall.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 
 doas pacman -Syu apparmor pacman-contrib wl-clipboard sway-contrib otf-font-awesome
-doas pacman -Syu alsa-utils pipewire tree man which jq rsync
+doas pacman -Syu alsa-utils pipewire pipewire-pulse tree man which jq rsync
 
 doas pacman -Syu chrony
 sys enable --now chronyd
@@ -13,4 +13,8 @@ sys start iwd
 
 doas pacman -Syu guile make base-devel devtools tig
 doas pacman -Syu impala id3v2 ffmpeg xdg-desktop-portal-wlr
+
+mkdir -p ~/.local/share/tig
+mkdir -p ~/.config/mozilla
+mkdir -p ~/.config/git
 
