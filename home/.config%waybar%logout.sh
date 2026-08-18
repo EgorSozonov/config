eca74d7b0d64436f9b2fc68407395fd79c13cfe7
EgorSozonov <Egor.Sozonov.09@gmail.com>
2026-08-18 16:14:58 +0300

    Foot color

--- home/.config%waybar%logout.sh
+++ home/.config%waybar%logout.sh
@@ -2,7 +2,7 @@
 
 menuResult=$(printf "Suspend\nLog out\nCancel" \
    | fzf --color="fg+:#000000,bg+:#00ff00,pointer:#00ff00,prompt:#00ff00,gutter:#000000" \
-      --no-info --layout=reverse --prompt="Sway Menu: " \
+      --no-info --layout=reverse --prompt=" [[[Sway Menu]]] " \
 )
 case "$menuResult" in
     "Suspend") aplay -q ~/music/xpMus/shutdown.wav; systemctl suspend ;;
