b66e228106b14aa347bce1ba43bbd24126c797a0
EgorSozonov <Egor.Sozonov.09@gmail.com>
2026-08-22 15:43:58 +0300

    Waybar optimizations

--- home/.config%waybar%logout.sh
+++ home/.config%waybar%logout.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/bash
 
 menuResult=$(printf "Suspend\nLog out\nCancel" \
-   | fzf --color="fg+:#000000,bg+:#00ff00,pointer:#00ff00,prompt:#00ff00,gutter:#000000" \
-      --no-info --layout=reverse --prompt=" [[[Sway Menu]]] " \
+   | fzf --color="fg+:#000000,bg+:#80c000,pointer:#00ff00,prompt:#80c000,gutter:#000000" \
+      --no-info --layout=reverse --prompt="       Sway Menu" \
 )
 case "$menuResult" in
     "Suspend") aplay -q ~/music/xpMus/shutdown.wav; systemctl suspend ;;
