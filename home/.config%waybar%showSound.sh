54b33fa6f2d7004a101fd63d124ba1ed1ea9fa2c
EgorSozonov <Egor.Sozonov.09@gmail.com>
2026-09-02 17:25:06 +0300

    Removed waybar

--- home/.config%waybar%showSound.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/bash
-if  amixer sget Master | grep -q '\[on\]' ; then
-    echo '{"text": "", "class": "on", "tooltip": "Sound is ON"}'
-else
-    echo '{"text": "", "class": "off", "tooltip": "Muted"}'
-fi
