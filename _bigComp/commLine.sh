b3fe5325fd27941bd17a48e790160a22be8ad46f
EgorSozonov <Egor.Sozonov.09@gmail.com>
2026-09-07 20:01:07 +0300

    Void: aliases

--- _bigComp/commLine.sh
+++ _bigComp/commLine.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/bash
 
+#One-time script to populate the kernel command line with the active storate UUID
+#Must substitute /dev/sda1 below with the root partition!
+
 doas mkdir /etc/cmdline.d
 doas chmod 755 /etc/cmdline.d
 doas echo "root=UUID=$(doas blkid -s UUID -o value /dev/sda1)" > root.conf
