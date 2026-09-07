8e2e39cea20458f37bf86af5de56c8135850100d
EgorSozonov <Egor.Sozonov.09@gmail.com>
2026-09-07 19:25:54 +0300

    Minor

--- s/void.sh
+++ s/void.sh
@@ -1,4 +1,4 @@
-cat s/userService | sed "s|username|$(id -un)|g" userService > run
+cat s/userService | sed "s|username|$(id -un)|g" s/userService > run
 doas mkdir -p /etc/sv/$(id -un)
 doas mv run /etc/sv/$(id -un)/run
 
